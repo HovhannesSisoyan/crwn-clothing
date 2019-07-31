e54bb982a0916de1d113ac088a4dd54c3e06b147
HovhannesSisoyan <hovosisoyan@gmail.com>
2019-07-31 12:56:39 +0400

    shop page created

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './page/homepage/homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import ShopPage from './page/shoppage/shopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
