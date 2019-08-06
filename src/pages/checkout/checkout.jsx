a7210020b279789f61beb22628f47c30ec051544
HovhannesSisoyan <hovosisoyan@gmail.com>
2019-08-06 22:32:11 +0400

    stripe is integrated

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item'; 
 import './checkout.scss'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -34,9 +35,13 @@ const CheckoutPage = ({ cartItems, total }) => (
                 )
             )
             }
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
-        </div>    
+        <div className='total'> TOTAL: ${total}</div>
+        <div className='test-warning'>
+        *Please use the fallowing test credit card for payments*
+        <br/>
+        4242 4242 4242 4242 -Exp: 01/20 - CVV: 123
+        </div>   
+        <StripeCheckoutButton price={total}/>
     </div>
 );
 
